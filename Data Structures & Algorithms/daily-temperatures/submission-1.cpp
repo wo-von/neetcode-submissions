cd81be806bb5497a71ad2140d908ff3e8ba75fdf
Sina Hosseinizad <24656395+wo-von@users.noreply.github.com>
2026-06-27 18:38:02 +0200

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<pair<int, int>> stk;
+        
+        vector<int> result(temperatures.size());
+        
+        for (int i = 0; i < temperatures.size(); i++){
+            int temp = temperatures[i];
+            
+            while (!stk.empty() && temp > stk.top().first){
+                auto curr = stk.top(); stk.pop();
+                result[curr.second] = i - curr.second;
+                
+            }
+            stk.push({temp, i});
+        }
+        return result;
+    }    
+};
